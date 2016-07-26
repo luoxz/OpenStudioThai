fba0a4d008c22515808c79fb0acf2177f340d187
pakorn <great.gwon@gmail.com>
2016-07-26 11:10:30 +0700

    Merge surface and SubSurface V2.

--- openstudiocore/src/bec/ForwardTranslator.cpp
+++ openstudiocore/src/bec/ForwardTranslator.cpp
@@ -936,7 +936,7 @@ void ForwardTranslator::doSectionOfWall(const model::Model &model, QDomElement &
                         createTagWithText(SectionL, "SectionListDescription", boundCon);
 
                         QDomElement SectionD = createTagWithText(SectionDetail,"SectionD");
-                        createTagWithText(SectionD, "SectionDetailSectionListName", ssfName);
+                        createTagWithText(SectionD, "SectionDetailSectionListName", sfName);
 
                         QString componentName = sub.construction().get().name().get().c_str();
                         boost::optional<model::ConstructionBase> scon = sub.construction();
